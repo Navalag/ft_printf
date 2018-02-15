7dae3fb4a2379ab5d2f97bf830baff1f41dfe1e8
Andrew GALAVAN <agalavan@student.unit.ua>
2018-02-15 11:20:25 +0200

    fixed bugs with 0 value for 'o', 'x' conv

--- src/print_res_func_2.c
+++ src/print_res_func_2.c
@@ -22,13 +22,9 @@ int		print_D_d_i_conversions(va_list ap)
 	width = set_width(1);
 	value = cast_D_d_i_size(ap);
 	if (value[0] == '0' && g_head->precision_flag == 1
-		&& g_head->precision == 0)
-	{
-		res = width;
-		free(value);
-	}
-	else
-		res = set_flag_for_d_i_u(generate_res_for_int(width, value));
+			&& g_head->precision == 0)
+		value[0] = '\0';
+	res = set_flag_for_d_i_u(generate_res_for_int(width, value));
 	res_len = ft_strlen(res);
 	ft_putstr(res);
 	clean_memory_leaks(res);
@@ -44,44 +40,14 @@ int		print_u_U_o_O_x_X_conversion(va_list ap, int base, int up_case)
 
 	width = set_width(1);
 	value = cast_u_U_o_O_x_X_size(ap, base, up_case);
-	// if ((g_head->conver_letter == 'O' || g_head->conver_letter == 'o') &&
-	// 		(g_head->precision_flag == 1 && g_head->precision == 0 &&
-	// 			g_head->flag_hesh == 1 && value[0] == '0'))
-	// 	res = set_flag_for_o_x_X(generate_res_for_int(width, ft_strdup("0")));
-	// if (value[0] == '0')
-	// 	g_head->flag_hesh = 0;
-	// if (g_head->precision_flag == 1 && g_head->precision == 0 &&
-	// 			value[0] == '0')
-	// 	res = set_flag_for_o_x_X(generate_res_for_int(width, ft_strdup("")));
-	// else
-	// 	res = set_flag_for_o_x_X(generate_res_for_int(width, value));
 	if (value[0] == '0' && g_head->conver_letter != 'p')
 	{
-		// res = check_exeptions(width);
-		if ((g_head->conver_letter == 'O' || g_head->conver_letter == 'o') &&
-			(g_head->precision_flag == 1 && g_head->precision == 0 &&
-			g_head->flag_hesh == 1))
-		{
-			ft_putchar('0');
-			free(width);
-			free(value);
-			return (1);
-		}
-		else if (g_head->precision_flag == 1 && g_head->precision == 0)
-		{
-			res = width;
-			free(value);
-		}
-		else
-		{
-			ft_putchar('0');
-			free(width);
-			free(value);
-			return (1);
-		}
+		if (g_head->precision_flag == 1 && g_head->precision == 0)
+			value[0] = '\0';
+		if (g_head->conver_letter == 'x' || g_head->conver_letter == 'X')
+			g_head->flag_hesh = 0;
 	}
-	else
-		res = set_flag_for_o_x_X(generate_res_for_int(width, value));
+	res = set_flag_for_o_x_X(generate_res_for_int(width, value));
 	res_len = ft_strlen(res);
 	ft_putstr(res);
 	clean_memory_leaks(res);
